ea866d96fd50484ed72988331440b05da009f402
bossclement <bosstclement@gmail.com>
2023-02-16 15:38:13 -0800

    New commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#includes <stdio.h>
+
+/**
+ * main - main function
+ * Description: my description
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (0);
+}
