58f30e1d80b1a0c7876c902edb255aa7d5f7b8dd
bossclement <bosstclement@gmail.com>
2023-02-16 13:56:46 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,8 @@
 /*
  * main - my main function
  *
+ * Description: this prints some string
+ * and returns 0.
  * Return: always returns 0 (success)
  *
  */
@@ -10,5 +12,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	returns (0);
+	return (0);
 }
