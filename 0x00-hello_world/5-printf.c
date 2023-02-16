9cc183182bec6a7a988b2afd869cf52087968861
bossclement <bosstclement@gmail.com>
2023-02-16 13:50:47 -0800

    New commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - my main function
+ *
+ * Return: always returns 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	returns (0);
+}
