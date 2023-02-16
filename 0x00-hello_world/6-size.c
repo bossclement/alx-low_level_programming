3b3a48e3256a294b41f3d7dde40862ef1d69aebe
bossclement <bosstclement@gmail.com>
2023-02-16 14:34:55 -0800

    New commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,16 @@
 
 /**
  * main - my main function
- *
- * Description: this prints all size
- * of data types of this pc
- * Return: always returns 0 (success)
- *
+ * Description: this is description of this function
+ * Return: always returns 0
  */
 
-int main(void) 
+int main(void)
 {
-        printf("Size of char: %zu byte\n", sizeof(char));
-        printf("Size of int: %zu bytes\n", sizeof(int));
-        printf("Size of long int: %zu bytes\n", sizeof(long int));
-    	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-    	printf("Size of float: %zu bytes\n", sizeof(float));
-
-    	return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
