723901c34f951c28a8dcb7321a02830d9bf1cf72
bossclement <bosstclement@gmail.com>
2023-02-17 13:07:34 -0800

    New commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10;)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
