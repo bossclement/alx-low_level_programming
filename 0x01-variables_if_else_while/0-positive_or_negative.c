bf29f566883d5b88ff79369e02abdce840a2a507
bossclement <bosstclement@gmail.com>
2023-02-17 12:22:58 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,28 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 
 /**
- * main - my main function
- *
- * Description: this prints all size
- * of data types of this pc
- * Return: always returns 0 (success)
- *
+ * main - main function
+ * Description: my description
+ * Return: always zero
  */
+
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-    {
-        printf("is positive");
-    } else if (n < 0)
-    {
-        printf("is negative");
-    } else
-    {
-        printf("is zero");
-    }
+	{
+		printf("is positive");
+	} else if (n < 0)
+	{
+		printf("is negative");
+	} else
+	{
+		printf("is zero");
+	}
 	return (0);
 }
