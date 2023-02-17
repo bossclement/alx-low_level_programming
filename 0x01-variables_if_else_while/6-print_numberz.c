0090c0fbc7aed426477be7f76f87e82d0485c6d6
bossclement <bosstclement@gmail.com>
2023-02-17 13:12:34 -0800

    New commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
