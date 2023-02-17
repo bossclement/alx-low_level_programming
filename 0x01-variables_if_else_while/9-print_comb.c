e221bc16286774801c119a63d49ca3575874da3d
bossclement <bosstclement@gmail.com>
2023-02-17 13:39:55 -0800

    New commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	int num1;
+
+	int num2;
+
+	for (num1 = 48; num1 <= 57; num1++)
+	{
+		for(num2 = 48; num2 <= 57; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			putchar(',');
+			putchat(' ');
+		}
+	}
+	return (0);
+}
