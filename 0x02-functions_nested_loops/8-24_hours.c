81f4e2956b12bfe00007479a7d146d44b370fd96
bossclement <bosstclement@gmail.com>
2023-02-21 12:44:46 -0800

    New commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,29 +3,29 @@
 /**
  * jack_bauer - main fuction
  * Description: my description
- * @n: hello
  * Return: 0
  */
 
 void jack_bauer(void)
 {
-	int num1, num2;
+	char num1, num2;
 
-	for (num1 = 0; num1 <= 23; num1++)
+	for (num1 = '0'; num1 <= '23'; num1++)
 	{
-		for (num2 = 0; num2 <= 59; num2++)
+		for (num2 = '0'; num2 <= '59'; num2++)
 		{
-			if (num1 < 10)
+			if (num1 < '10')
 			{
 				_putchar('0');
 			}
-			_putchar(num1 + 0);
+			_putchar(num1);
 			_putchar(':');
-			if (num2 < 10)
+			if (num2 < '10')
 			{
 				_putchar('0');
 			}
-			_putchar(num2 + '0');
+			_putchar(num2);
+			_putchar('\n');
 		}
 	}
 }
