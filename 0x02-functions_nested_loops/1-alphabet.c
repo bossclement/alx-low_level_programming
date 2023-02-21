5a0d7c183a645005992b798a6600d4d899115b79
bossclement <bosstclement@gmail.com>
2023-02-21 10:26:11 -0800

    new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main fuction
+ * Description: my description
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+}
