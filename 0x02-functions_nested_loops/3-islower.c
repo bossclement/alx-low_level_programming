4b75f1892a4596583c6dc86d688c0117b32f4c15
bossclement <bosstclement@gmail.com>
2023-02-21 11:10:01 -0800

    New commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,12 +11,12 @@ int _islower(int c)
 {
 	int a;
 
-	for (a = 65; a <= 90; a++)
+	for (a = 97; a <= 122; a++)
 	{
 		if (c == a)
 		{
-			return (0);
+			return (1);
 		}
 	}
-	return (1);
+	return (0);
 }
