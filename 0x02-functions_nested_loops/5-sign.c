db8bf2331be19aea4d31be6b641fe90f52df3ba3
bossclement <bosstclement@gmail.com>
2023-02-21 11:30:26 -0800

    New commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - main fuction
+ * Description: my description
+ * @n: hello
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	switch(n)
+	{
+		case n > 0:
+			_putchar('+');
+			return (1);
+		case n == 0:
+			_putchar(48);
+			return (0);
+		default:
+			_putchar('-');
+			return (-1);
+	}
+}
