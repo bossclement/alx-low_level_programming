47c1f4fe719a9a7dda1b92e0a4b45f91e21616b2
bossclement <122975092+bossclement@users.noreply.github.com>
2023-02-21 23:24:16 +0200

    New commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,55 +1,46 @@
 #include "main.h"
-#include <math.h>
-#include <stdio.h>
 
 /**
- * get_first_digit - hello
- * @num: yes
- * Description: yes
- * Return: 0
+ * jack_bauer - Hello
+ * Description: world
+ * Return: void
  */
-
-int get_first_digit(int num)
-{
-	int digits = (int)log10(num);
-
-	return (num / pow(10, digits));
-}
-
-
-/**
- * jack_bauer - main fuction
- * Description: my description
- * Return: 0
- */
-
 void jack_bauer(void)
 {
-	char num1, num2;
+	int a, b, c, d, e;
 
-	for (num1 = 0; num1 <= 23; num1++)
+	e = 58;
+	a = '0';
+	while (a < '3')
 	{
-		for (num2 = 0; num2 <= 59; num2++)
+		if (a == '2')
 		{
-			if (num1 < 10)
-			{
-				_putchar('0');
-				_putchar(num1 + '0');
-			} else
-			{
-				_putchar(get_first_digit(num1) + '0');
-				_putchar(num1 % 10 + '0');
-			}
-			if (num2 < 10)
-			{
-				_putchar('0');
-				_putchar(num2 + '0');
-			} else
-			{
-				_putchar(get_first_digit(num2) + '0');
-				_putchar(num2 % 10 + '0');
-			}
-			_putchar('\n');
+			e = '4';
 		}
+		b = '0';
+		while (b < e)
+		{
+			c = '0';
+			while (c < '6')
+			{
+				d = '0';
+				while (d < 58)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+					d++;
+				}
+				d = '0';
+				c++;
+			}
+			c = '0';
+			b++;
+		}
+		b = '0';
+		a++;
 	}
 }
