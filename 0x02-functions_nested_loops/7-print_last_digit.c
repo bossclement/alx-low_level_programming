c70b8aefea4c32ceee5f4a46bba92d3c8066b79e
bossclement <bosstclement@gmail.com>
2023-02-21 11:49:28 -0800

    New commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - main fuction
+ * Description: my description
+ * @n: hello
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
