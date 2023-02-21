19d2197fc85c36431633f209f4e9c5220b055b3e
bossclement <bosstclement@gmail.com>
2023-02-21 15:03:29 -0800

    New commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - main fuction
+ * Description: my description
+ * @n: hello
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d", n);
+		if (n < 98)
+		{
+			printf(" ,")
+		}
+		n++;
+	}
+}
