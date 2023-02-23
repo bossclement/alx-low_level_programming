1748e336287312e150bb8304b1818383ba4d8b62
bossclement <bosstclement@gmail.com>
2023-02-24 01:55:31 +0200

    New commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+# include "main.h"
+
+/**
+ * print_triangle - my func
+ * Description: hello
+ * @size: yo
+ * Returns: number
+ */
+
+void print_triangle(int size)
+{
+	int prints;
+
+	if (size <= 0)
+	{
+		return;
+	}
+
+	for (prints = 0; prints < size; prints++)
+	{
+		int x = size - prints;
+		
+		while (--x)
+		{
+			_putchar(' ');
+		}
+		for (x = 0; x <= prints; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+
+	}
+}
