be6bf3791d77f517efa36028083ccc2f12bb8b54
bossclement <bosstclement@gmail.com>
2023-02-24 01:15:01 +0200

    New commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ * Description: Hello
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		} else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", num);
+		}
+		printf("\n");
+	}
+	return (0);
+}
