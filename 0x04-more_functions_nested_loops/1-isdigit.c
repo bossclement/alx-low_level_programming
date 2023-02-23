4a114e7bfafb256ed1e06a5e712c808df900c978
bossclement <bosstclement@gmail.com>
2023-02-23 09:36:04 -0800

    New commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+/**
+ * _isupper - my function
+ * @c: arguments
+ * Description: the description
+ * Return: hello
+ */
+
+int _isupper(int c)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
