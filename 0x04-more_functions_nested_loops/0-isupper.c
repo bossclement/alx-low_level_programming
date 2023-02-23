89a13bfce2f11ea651bdbd01f468348168cdab83
bossclement <bosstclement@gmail.com>
2023-02-23 09:30:59 -0800

    New commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/**
+ * _isupper - my function
+ * @c: arguments
+ * Description: the description
+ * Return: hello
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 65; i <= 90; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
