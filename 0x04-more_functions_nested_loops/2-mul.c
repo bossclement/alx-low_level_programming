64d1cb8a9eaa09185cb70bf06025a8db420ea8ff
bossclement <bosstclement@gmail.com>
2023-02-23 22:10:14 +0200

    hello

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
+/**
+ * mul - my function
+ * @a: arguments
+ * @b: yes
+ * Description: the description
+ * Return: hello
+ */
+
 int mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
