74506b587615f2e1c1e71241c5c481f7093b0451
bossclement <bosstclement@gmail.com>
2023-02-28 02:47:21 +0200

    New commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,10 @@
 void puts_half(char *str)
 {
 	int length = 0;
-	char n;
 
 	while (str[length] != '\0')
 	{
 		length++;
-	}
-
-	n = (length - 1) / 2;
-
-	if (length % 2 != 0)
-	{
-		_putchar(n);
-		return;
 	}
 
 	length = (length / 2) + 1;
