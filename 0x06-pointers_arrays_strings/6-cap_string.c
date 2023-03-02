6737862f51e1db524ff123d9624ad1283cf61832
bossclement <bosstclement@gmail.com>
2023-03-03 01:06:16 +0200

    New commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,11 +14,14 @@ char *cap_string(char *str)
 
 	while (str[i++])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && ((prev_c < 'a' || prev_c > 'z') || (prev_c < 'A' || prev_c > 'Z')))
+		if ((str[i] >= 'a' && str[i] <= 'z'))
 		{
-			prev_c = str[i];
-			str[i] -= 32;
-			continue;
+			if ((prev_c < 'a' || prev_c > 'z') || (prev_c < 'A' || prev_c > 'Z'))
+			{
+				prev_c = str[i];
+				str[i] -= 32;
+				continue;
+			}
 		}
 		prev_c = str[i];
 	}
