ba990051dccadf5c05f2396aac3f1ed4aeea8cbd
bossclement <bosstclement@gmail.com>
2023-03-06 22:54:45 +0200

    New commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - function to find a character in an array
+ * @s: array
+ * @c: character
+ * Description: find a character
+ * Return: memory address of the character ( pointer )
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (s[i++])
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+}
