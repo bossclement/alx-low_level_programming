586bbdaacd87251c6ffbbb605defc705dc74dc77
bossclement <bosstclement@gmail.com>
2023-03-10 23:37:06 +0200

    New commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints program file path
+ * @agrc: number of arguments
+ * @argv: arguments
+ */
+
+int main(int agrc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
