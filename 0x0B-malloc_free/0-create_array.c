defa4e154969826bbb7dbddb2e9f9e13a630f207
bossclement <bosstclement@gmail.com>
2023-03-13 21:18:31 +0200

    New commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * create_array - function
+ * @size: param 1
+ * @c: param 2
+ * Return: a pointer or Null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arrayP = (char *)malloc(size);
+
+	if (size == 0)
+		return ('\0');
+	arrayP[0] = c;
+	return (arrayP);
+}
+
