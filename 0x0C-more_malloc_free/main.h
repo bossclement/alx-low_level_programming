b0cd2712e3d621caded6431f4954f231e73557af
bossclement <bosstclement@gmail.com>
2023-03-16 22:44:03 +0200

    New commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,5 @@ void *malloc_checked(unsigned int b);
 
 #endif
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
