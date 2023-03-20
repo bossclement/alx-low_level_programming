fd02a1b00859d1b499a0090b91a68659438d1d94
bossclement <bosstclement@gmail.com>
2023-03-20 23:18:09 +0200

    New commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
 
-
-
 /**
  * init_dog - initializer to my structure
  * @d: the structure
@@ -12,7 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
