46a89ff51e8f00b2437e6bf40ba1707eb75eb983
bossclement <bosstclement@gmail.com>
2023-03-21 00:24:45 +0200

    New message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 
- char *strcp(char *dest, char *src);
- int _strlen(char *str);
+char *strcp(char *dest, char *src);
+int _strlen(char *str);
 
 /**
  * _strlen - gets string length
@@ -13,59 +13,60 @@
 int _strlen(char *str)
 {
 	int len = 0;
-	
+
 	if (str != NULL)
 	{
 		while (str[len])
 			len++;
 		return (len);
 	}
-	
+
 	return (0);
 }
- 
+
 /**
  * strcp - copies a string
  * @dest: destination
  * @src: source
  * Return: pointer to a string
  */
- 
- char *strcp(char *dest, char *src)
- {
- 	int str_len = _strlen(src), i = 0;
- 	
- 	for (;i < str_len; i++)
- 		dest[i] = src[i];
- 	
- 	return (dest);
- }
+
+char *strcp(char *dest, char *src)
+{
+	int str_len = _strlen(src), i = 0;
+
+	for ( ; i < str_len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
 
 /**
  * new_dog - initializer to my structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Return: pointer
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-		
+
 	d->name = malloc(sizeof(char) * _strlen(name));
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	
+
 	d->owner = malloc(sizeof(char) * _strlen(owner));
 	if (d->owner == NULL)
 	{
@@ -77,6 +78,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = strcp(d->name, name);
 	d->age = age;
 	d->owner = strcp(d->owner, owner);
-	
+
 	return (d);
 }
