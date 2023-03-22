d9af916464e907a146292fba4dd25775dc432f7a
bossclement <bossclement@gmail.com>
2023-03-22 22:45:55 +0200

    New commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: my array
+ * @size: number
+ * @action: pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
+
