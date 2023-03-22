a8e179ec046a5e680b85fc13c6f1474626ecab61
bossclement <bossclement@gmail.com>
2023-03-22 22:34:33 +0200

    New commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
 
 #endif
