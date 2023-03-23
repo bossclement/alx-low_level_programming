096c7b22bf8d23dc2df48b98198eafb894c89335
bossclement <bossclement@gmail.com>
2023-03-23 03:12:27 +0200

    New commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,9 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-	int (*operation)(int, int);
+
 	int num1, num2;
-	char *operator;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -20,21 +20,23 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
-	operator = argv[2];
 
-	if ((*operator == '/' || *operator  == '%') && num2 == 0)
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	operation = get_op_func(operator);
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	printf("%d\n", operation(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
