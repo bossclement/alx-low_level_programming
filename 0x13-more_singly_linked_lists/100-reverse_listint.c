0f97f02be63f742c66d196a351da955e819f74d1
bossclement <bossclement@gmail.com>
2023-04-04 22:00:37 +0200

    New commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse nodes in a list
+ * @head: a starting point
+ * Return: return a pointer to reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ahead, *behind;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	behind = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		ahead = (*head)->next;
+		(*head)->next = behind;
+		behind = *head;
+		*head = ahead;
+	}
+
+	(*head)->next = behind;
+
+	return (*head);
+}
