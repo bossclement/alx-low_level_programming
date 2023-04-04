6efd23b8585da7c2c01231fa3258c7838d9814f6
bossclement <bossclement@gmail.com>
2023-04-04 15:09:46 +0200

    New commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,14 @@
  * @h: the list
  * Return: the number of nodes inside the list
  */
- 
+
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
 	if (h == NULL)
 		return (nodes);
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
@@ -21,5 +21,4 @@ size_t print_listint(const listint_t *h)
 		nodes++;
 	}
 	return (nodes);
-		
 }
