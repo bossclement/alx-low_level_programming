241898081decfa2750070b7bdadb6f4f2bbb0bf0
bossclement <bossclement@gmail.com>
2023-04-09 21:03:17 +0200

    New commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * clear_bit - sets the value of a bit to 1 at a given index.
+ * @n: The number
+ * @index: Index position of the bit to change
+ * Return: 1 on success -1 on fail
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(n) * 8))
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
