3ded03584d652086fc38027208f742055783ab0f
bossclement <bossclement@gmail.com>
2023-04-10 19:04:11 +0200

    New commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_file - creates a file.
+ * @filename: file path where to save the file
+ * @text_content: content to write in the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, content_len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[content_len] != '\0')
+		content_len++;
+	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, 0600);
+	if (fd == -1)
+	{
+		if (truncate(filename, content_len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	else if (write(fd, text_content, content_len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
