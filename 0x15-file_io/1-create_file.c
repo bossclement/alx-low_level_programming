fe7ddabca00cf88dea357110a8253ed3a86d5b7e
bossclement <bossclement@gmail.com>
2023-04-10 19:37:42 +0200

    New commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,6 +27,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, 0600);
 	if (fd == -1)
 	{
+		close(fd);
 		if (truncate(filename, content_len) == -1)
 		{
 			close(fd);
