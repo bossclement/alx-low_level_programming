6514d2542ad889566b694542aa7f381a6102ed32
bossclement <bossclement@gmail.com>
2023-05-18 23:13:35 +0200

    New commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new == NULL)
 		return (NULL);
+	if (head == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
 	new->n = n;
 	new->next = *head;
