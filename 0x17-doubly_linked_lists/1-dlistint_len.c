de05a488b5bbfd5448ec63504ea1f3c72a3d8c36
bossclement <bossclement@gmail.com>
2023-05-18 23:10:48 +0200

    New commit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * dlistint_len - returns number of nodes
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - adds a node at the beginning.
+ * @head: starting point of the list.
+ * @n: number of the new node.
+ * Return: pointer to the new node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t *));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (new);
+}
