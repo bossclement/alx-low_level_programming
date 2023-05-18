51781c917fc093cd69db0f4976be36ede20269cf
bossclement <bossclement@gmail.com>
2023-05-18 22:53:58 +0200

    New commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * dlistint_len - returns number of nodes
+ * @h: starting point of the list
+ * Return: number of nodes in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t *cur = (dlistint_t *)h;
+	size_t counts = 0;
+	
+	while (cur && !(cur == h && counts > 0))
+	{
+		cur = cur->next;
+		counts++;
+	}
+	return (counts);
+}
