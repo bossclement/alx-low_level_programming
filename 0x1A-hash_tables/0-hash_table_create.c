a79a6f0765393f05999ea9ac757fe8d3ddec065e
bossclement <bossclement@gmail.com>
2023-06-09 22:32:29 +0200

    New commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,23 +20,31 @@ void free_pointer(void *p)
  * Return: Pointer to the newly created hash table.
 */
 
+#include <stdlib.h>
+#include "hash_tables.h"
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
-	hash_node_t **node_array = malloc(sizeof(hash_node_t) * (size + 1));
-	unsigned long int i;
+    hash_table_t *hash_table;
+    unsigned long int i;
 
-	if (new_table == NULL || node_array == NULL)
-	{
-		free_pointer((void *) new_table);
-		free_pointer((void *) (*node_array));
-		return (NULL);
-	}
-	
-	for (i = 0; i < size + 1; i++)
-		node_array[i] = NULL;
+    hash_table = malloc(sizeof(hash_table_t));
+    if (hash_table == NULL)
+        return NULL;
 
-	new_table->array = node_array;
-	return (new_table);
+    hash_table->array = malloc(sizeof(hash_node_t *) * size + 1);
+    if (hash_table->array == NULL)
+    {
+        free(hash_table);
+        return NULL;
+    }
+
+    for (i = 0; i < size; i++)
+        hash_table->array[i] = NULL;
+
+    hash_table->size = size;
+
+    return hash_table;
 }
+
 
