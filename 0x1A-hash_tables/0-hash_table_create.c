a5dee74522b87d2498d269cfca7883260ea63b6c
bossclement <bossclement@gmail.com>
2023-06-09 21:55:37 +0200

    New commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ void free_pointer(void *p)
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
-	hash_node_t *node_array = malloc(sizeof(hash_node_t) * size + 1);
+	hash_node_t *node_array = malloc(sizeof(hash_node_t) * size);
 
 	if (new_table == NULL || node_array == NULL)
 	{
