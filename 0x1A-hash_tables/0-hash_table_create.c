c14244e312d57debc2945dc321df9ca66d75dbfb
bossclement <bossclement@gmail.com>
2023-06-09 21:54:07 +0200

    New commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+
+/**
+ * free_pointer - frees merory allocated in the heap.
+ * @p: pointer to that memory
+*/
+
+void free_pointer(void *p)
+{
+	if (p != NULL)
+		free(p);
+}
+
+/**
+ * hash_table_create - function that creates hash table.
+ * @size: the size of the array.
+ * Return: Pointer to the newly created hash table.
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	hash_node_t *node_array = malloc(sizeof(hash_node_t) * size + 1);
+
+	if (new_table == NULL || node_array == NULL)
+	{
+		free_pointer((void *) new_table);
+		free_pointer((void *) node_array);
+		return (NULL);
+	}
+
+	new_table->array = &node_array;
+	return (new_table);
+}
+
