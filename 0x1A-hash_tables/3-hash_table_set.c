a5f090636738f4e73112651d2f898f2927bf6d79
bossclement <bossclement@gmail.com>
2023-06-23 23:57:24 +0200

    New commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,17 +3,6 @@
 #include <stdlib.h>
 
 /**
- * free_node - frees memory of ht node.
- * @node: the node to free.
- */
-void free_node(hash_node_t *node)
-{
-	free(node->key);
-	free(node->value);
-	free(node);
-}
-
-/**
  * hash_table_set - adds an element to my hash table.
  * @ht: is the hash table.
  * @key: key of the element to add.
@@ -22,12 +11,25 @@ void free_node(hash_node_t *node)
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *prev, *prevv = NULL;
+	hash_node_t *node, *temp;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
+	index = key_index((unsigned char *) key, ht->size);
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+	
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
@@ -46,30 +48,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	node->next = NULL;
-
-	index = key_index((unsigned char *) node->key, ht->size);
-	prev = ht->array[index];
-
-	if (prev == NULL)
-	{
-		ht->array[index] = node;
-		return (1);
-	}
-
-	while (prev->next)
-	{
-		if (strcmp(prev->key, node->key) == 0)
-		{
-			free_node(prev);
-			if (prevv != NULL)
-				prevv->next = node;
-			else
-				ht->array[index] = node;
-			return (1);
-		}
-		prevv = prev;
-		prev = prev->next;
-	}
 
 	node->next = ht->array[index];
 	ht->array[index] = node;
