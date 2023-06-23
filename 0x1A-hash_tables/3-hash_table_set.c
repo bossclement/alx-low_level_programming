daf89454ce126a3ab9132c13bd20e1a7a471c440
bossclement <bossclement@gmail.com>
2023-06-23 23:16:51 +0200

    New commit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * hash_table_set - adds an element to my hash table.
@@ -10,24 +11,34 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *node, *prev;
 	unsigned long int index;
-	hash_node_t *prev;
 
 	if (strlen(key) == 0)
 		return (0);
-	
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 
 	node->key = strdup(key);
 	node->value = strdup(value);
-	
+
 	if (node->key == NULL || node->value == NULL)
 		return (0);
 
-	index = key_index(node->key, ht->size);
-	
+	index = key_index((unsigned char *) node->key, ht->size);
 	prev = ht->array[index];
+
+	if (prev == NULL)
+	{
+		ht->array[index] = node;
+		return (1);
+	}
+
+	while (prev->next)
+		prev = prev->next;
+
+	prev->next = node;
+	return (1);
 }
