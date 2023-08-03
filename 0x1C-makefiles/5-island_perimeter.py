47e52ad706e80a297e6e5f21925b1629484f57f3
bossclement <bossclement@gmail.com>
2023-08-03 23:24:59 +0200

    New commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,23 +5,22 @@ Module that has a function that returns perimeter of the island.
 
 
 def island_perimeter(grid):
-	"""Computes perimeter of an island
-	Args:
-		grid (list): list of list of integers
-	Returns:
-		int: the perimeter of the island
-	"""
-	perimeter = 0
-	rows, cols = len(grid), len(grid[0])
+    """Computes perimeter of an island
+    Args:
+            grid (list): list of list of integers
+    Returns:
+            int: the perimeter of the island
+    """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
 
-	for row in range(rows):
-		for col in range(cols):
-			if grid[row][col] == 1:
-				perimeter += 4
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
 
-				if row > 0 and grid[row - 1][col] == 1:
-					perimeter -= 2
-				if col > 0 and grid[row][col - 1] == 1:
-					perimeter -= 2
-	return perimeter
-
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
