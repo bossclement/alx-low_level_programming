6c067333fc0f45c7a39423030881121b7244af1e
bossclement <bossclement@gmail.com>
2023-08-03 23:20:47 +0200

    New commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,9 +14,21 @@ def island_perimeter(grid):
 	width = 0
 	length = 0
 	for index, lst in enumerate(grid):
-		if lst.count(1) > width:
-			width = lst.count(1)
-		if index <= len(lst) - 1 and [i[index] for i in grid].count(1) > length:
-			length = [i[index] for i in grid].count(1)
+		list_length = []
+		list_width = []
+		for i in lst:
+			if list_width and not i:
+				break
+			list_width.append(i)
+		if index > len(lst) - 1:
+			break
+		for i in [x[index] for x in grid]:
+			if list_length and not i:
+				break
+			list_length.append(i)
+		if list_width.count(1) > width:
+			width = list_width.count(1)
+		if list_length.count(1) > length:
+			length = list_length.count(1)
 	return 2 * (width + length)
 
