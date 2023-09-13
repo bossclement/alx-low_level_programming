e412b9cbfef141f2ed507eaeba85be432d4e0dff
bossclement <bossclement@gmail.com>
2023-09-13 20:38:30 +0200

    New commit

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,42 +1,48 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 /**
- * interpolation_search - Search for a value in a sorted array using Interpolation Search
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: The value to search for
+ * interpolation_search - searches for a target in a sorted integer array
+ * using a specialized interpolation search technique
+ * @array: pointer to the initial element of the array
+ * @size: number of elements in the array
+ * @value: target value to find
  *
- * Return: Index of the value if found, -1 if not found or on error
+ * Return: the first index containing `val`, or -1 if `val` is not found or
+ * `arr` is NULL
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low = 0, high = size - 1, pos;
-    if (array == NULL)
-        return (-1);
+	size_t l = 0;
+	size_t h = sz - 1;
+	size_t p;
+	int *arr = array;
+	size_t sz = size;
+	int vl = value;
 
-    while (low <= high && value >= array[low] && value <= array[high])
-    {
-        if (low == high)
-        {
-            if (array[low] == value)
-                return (low);
-            return (-1);
-        }
+	if (!arr)
+		return (-1);
 
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-
-        if (array[pos] == value)
-            return (pos);
-
-        if (array[pos] < value)
-            low = pos + 1;
-        else
-            high = pos - 1;
-    }
-
-    printf("Value checked array[%lu] is out of range\n", low);
-    return (-1);
+	while ((arr[h] != arr[l]) && (val >= arr[l]) && (val <= arr[h]))
+	{
+		p = l + (((double)(h - l) / (arr[h] - arr[l]))
+			    * (val - arr[l]));
+		printf("Checked arr[%lu] = [%d]\n", p, arr[p]);
+		if (arr[p] < val)
+			l = p + 1;
+		else if (val < arr[p])
+			h = p - 1;
+		else
+			return (p);
+	}
+	if (val == arr[l])
+	{
+		printf("Checked arr[%lu] = [%d]\n", l, arr[l]);
+		return (l);
+	}
+	p = l + (((double)(h - l) / (arr[h] - arr[l]))
+		     * (val - arr[l]));
+	printf("Checked arr[%lu] is not in the expected range\n", p);
+	return (-1);
 }
 
